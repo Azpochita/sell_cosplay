e82ddc6d9d527e8984f8cf1a027a9884e82b957a
Aaron <aaa.aaronzhou@gmail.com>
2023-12-24 16:28:51 -0800

    commit

--- js/home.js
+++ js/home.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const productContainers = [...document.querySelectorAll('.product-container')];
+    const propContainers = [...document.querySelectorAll('.prop-container')];
     const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
     const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-    productContainers.forEach((item, i) => {
+    propContainers.forEach((item, i) => {
         let containerDimenstions = item.getBoundingClientRect();
         let containerWidth = containerDimenstions.width;
 
